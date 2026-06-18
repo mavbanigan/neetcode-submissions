2e33e7950ddb4b9ba212b6a1b39f7bce7f4e23b8
Maverick Banigan <154767908+mavbanigan@users.noreply.github.com>
2026-06-18 13:40:33 -0400

    Delete Data Structures & Algorithms/two-integer-sum/submission-5.py

--- Data Structures & Algorithms/two-integer-sum/submission-5.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i, num in enumerate(nums):
-            difference = target - num
-            if difference in map:
-                return [map[difference], i]
-            else:
-                map[num] = i
