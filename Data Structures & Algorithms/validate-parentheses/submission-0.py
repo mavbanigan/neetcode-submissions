db65777b7565f3d275d2cb7149f75fc2fd209383
Maverick Banigan <154767908+mavbanigan@users.noreply.github.com>
2026-06-22 16:48:13 -0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = [] # Initialize the stack
+        map = { ")" : "(", "]" : "[", "}" : "{"} # Initialize and define the hashmap
+        for char in s:
+            if char in map:
+                if stack and stack[-1] == map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return len(stack) == 0
+
+        
+        
